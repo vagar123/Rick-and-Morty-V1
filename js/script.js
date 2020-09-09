0cb88d870b7a5df60967b9fffdb04599eea98651
vagar123 <vanessa.garnica.14@gmail.com>
2020-09-09 14:44:51 -0500

    ajustes javascript

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
-function cambiarNombre1(){
+function cambiarNombre(idDiv){
     let mensaje = prompt("Cambia el nombre del personaje")
-    document.getElementById("t1").innerHTML = mensaje
-}
-
-function cambiarNombre2(){
-    let mensaje = prompt("Cambia el nombre del personaje")
-    document.getElementById("t2").innerHTML = mensaje
-}
-
-function cambiarNombre3(){
-    let mensaje = prompt("Cambia el nombre del personaje")
-    document.getElementById("t3").innerHTML = mensaje
-}
-
-function cambiarNombre4(){
-    let mensaje = prompt("Cambia el nombre del personaje")
-    document.getElementById("t4").innerHTML = mensaje
-}
-
-function cambiarNombre5(){
-    let mensaje = prompt("Cambia el nombre del personaje")
-    document.getElementById("t5").innerHTML = mensaje
-}
-
-function cambiarNombre6(){
-    let mensaje = prompt("Cambia el nombre del personaje")
-    document.getElementById("t6").innerHTML = mensaje
+    document.getElementById(idDiv).innerHTML = mensaje
 }
