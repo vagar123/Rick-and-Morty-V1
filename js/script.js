0aaf4548f716b1f986b01df1e9472156c64a542d
vagar123 <vanessa.garnica.14@gmail.com>
2020-09-08 19:33:52 -0500

    arreglos de estilo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 function cambiarNombre1(){
-    let mensaje = prompt("Escribe tu mensaje")
+    let mensaje = prompt("Cambia el nombre del personaje")
     document.getElementById("t1").innerHTML = mensaje
 }
 
 function cambiarNombre2(){
-    let mensaje = prompt("Escribe tu mensaje")
+    let mensaje = prompt("Cambia el nombre del personaje")
     document.getElementById("t2").innerHTML = mensaje
 }
 
 function cambiarNombre3(){
-    let mensaje = prompt("Escribe tu mensaje")
+    let mensaje = prompt("Cambia el nombre del personaje")
     document.getElementById("t3").innerHTML = mensaje
 }
 
 function cambiarNombre4(){
-    let mensaje = prompt("Escribe tu mensaje")
+    let mensaje = prompt("Cambia el nombre del personaje")
     document.getElementById("t4").innerHTML = mensaje
 }
 
 function cambiarNombre5(){
-    let mensaje = prompt("Escribe tu mensaje")
+    let mensaje = prompt("Cambia el nombre del personaje")
     document.getElementById("t5").innerHTML = mensaje
 }
 
 function cambiarNombre6(){
-    let mensaje = prompt("Escribe tu mensaje")
+    let mensaje = prompt("Cambia el nombre del personaje")
     document.getElementById("t6").innerHTML = mensaje
 }
